121707c1dd00b71ef0c31f40ff7f0bd1f4525de8
AITabby <aitabby@github.com>
2026-05-31 21:56:04 +0800

    clean: remove stray test files

--- inject_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-TEXT="$1"
-echo "$TEXT" | pbcopy
-osascript -e 'tell application "Codex" to activate' -e 'delay 0.2' -e 'tell application "System Events" to keystroke "v" using command down' -e 'delay 0.1' -e 'tell application "System Events" to key code 36'
-echo "Injected: $TEXT"
